dbca1b440d1af3b5d8381efeb033fcf779be4cb9
Himanshu Neema <himanshun.iitkgp@gmail.com>
2017-05-05 19:38:24 -0400

    new scripts

---
+++ infrastructure/kubernetes/common/delete_deployment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl delete deployment distributed-objects-app-deployment
--- infrastructure/kubernetes/common/deploy_app.sh
+++ infrastructure/kubernetes/common/deploy_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl run distributed-objects-app-deployment --image=gigapotential/distributed-objects-app:v0 --port=80
+kubectl run distributed-objects-app-deployment --image=gigapotential/distributed-objects-app:v0 --port=80 -l visualize=true,name=CSE775
